8089295ed372ee959cc87b57feb193d730474ae3
nekopanda <kouhiro3782@yahoo.co.jp>
2019-05-14 15:45:51 +0900

    fixing Link

--- src/js/client.js
+++ src/js/client.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Layout from "./pages/Layout";
+import Featured from "./pages/Featured";
+import Settings from "./pages/Setting";
+import Archives from "./pages/Archives";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout />, app);
+ReactDOM.render(
+    <Router>
+        <Layout>
+            <Route exact path="/" compornent={Featured}></Route>
+            <Route path="/archives" compornent={Archives}></Route>
+            <Route path="/settings" compornent={Settings}></Route>
+        </Layout>
+    </Router>,
+app);
